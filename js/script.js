2eb8d20576915c40aab9bebd0c2cffbb2b6ef734
Davef <76170782+davefdev@users.noreply.github.com>
2022-05-08 22:28:55 +0100

    css

--- js/script.js
+++ js/script.js
@@ -1,65 +1,20 @@
 /*===== SIDEBAR NAV =====*/
 
-const sidebarNav = document.querySelector(".sidebar-nav");
+let navbar = document.querySelector(".header .navbar");
 
-function openNav() {
-  sidebarNav.style.width = "350px";
-}
-
-function closeNav() {
-  sidebarNav.style.width = "0px";
-}
-
-document
-  .querySelector(".sidebar-close-btn")
-  .addEventListener("click", closeNav);
-
-document.querySelector(".hamburger").addEventListener("click", openNav);
-
-//
-
-/*===== HAMBURGER COLLAPSE =====*/
-const showMenu = (toggleId, navId) => {
-  const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId);
-
-  if (toggle && nav) {
-    toggle.addEventListener("click", () => {
-      nav.classList.toggle("show");
-    });
-  }
+document.querySelector("#hamburger").onclick = () => {
+  navbar.classList.toggle("active");
 };
 
-showMenu("nav-toggle", "nav-menu");
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
 
-/*===== REMOVE MENU MOBILE =====*/
-const navLink = document.querySelectorAll(".nav__link");
-
-function linkAction() {
-  const navMenu = document.getElementById("nav-menu");
-  navMenu.classList.remove("show");
-}
-navLink.forEach((n) => n.addEventListener("click", linkAction));
-
-//LOADER
-
-const fadeoutWrapper = document.querySelector(".loader-wrapper");
-
-function fadeout() {
-  const fade = setInterval(() => {
-    if (fadeoutWrapper.style.opacity === 0) {
-      fadeoutWrapper.style.opacity = 1;
-    }
-    if (fadeoutWrapper.style.opacity > 0) {
-      fadeoutWrapper.style.opacity -= 1;
-    }
-  }, 300);
-}
-
-window.addEventListener("load", function () {
-  setTimeout(() => {
-    fadeoutWrapper.style.display = "none";
-  }, 1000);
-});
-
-//H
+document
+  .querySelectorAll(".about-section .video-container .controls .control-btn")
+  .forEach((btn) => {
+    btn.onclick = () => {
+      let src = btn.getAttribute("data-src");
+      document.querySelector(".about-section .video-container .video").src = src;
+    };
+  });
